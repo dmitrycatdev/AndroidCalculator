f5c2c613ab3f5967776b2c0cf22b154e67e29f94
Dmitry Vorobiov <hellraiser666cat@gmail.com>
2018-01-18 16:24:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+    Double operand = null;
+    String lastOperation = "=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultField = (TextView) findViewById(R.id.resultField);
+        numberField = (EditText) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERAND", lastOperation);
+        if (operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand= savedInstanceState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+    // обработка нажатия на числовую кнопку
+    public void onNumberClick(View view){
+
+        Button button = (Button)view;
+        numberField.append(button.getText());
+
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+        }
+    }
+    // обработка нажатия на кнопку операции
+    public void onOperationClick(View view){
+
+        Button button = (Button)view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+        // если введенно что-нибудь
+        if(number.length()>0){
+            number = number.replace(',', '.');
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+    //очистка полей ввода и результата
+    public void onClearClick(View View) {
+        numberField.setText("");
+        resultField.setText("");
+        operationField.setText("");
+    }
+
+    private void performOperation(Double number, String operation){
+
+        // если операнд ранее не был установлен (при вводе самой первой операции)
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand=0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace(',', '.'));
+        numberField.setText("");
+    }
+}
